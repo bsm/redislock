c0575c92b8855dd9e009906f5223a058412c978d
Líng Yàomíng <86768220+spike014@users.noreply.github.com>
2023-03-22 21:20:58 +0800

    refactor(obtain): return ctx's error when ctx.Done() (#64)

--- redislock.go
+++ redislock.go
@@ -88,7 +88,7 @@ func (c *Client) Obtain(ctx context.Context, key string, ttl time.Duration, opt 
 
 		select {
 		case <-ctx.Done():
-			return nil, ErrNotObtained
+			return nil, ctx.Err()
 		case <-ticker.C:
 		}
 	}
