dae4db4eba07825f3c00de8f05a9dc9a8d193570
Benjamin Hallion <bhallion@ohbibi.com>
2023-09-06 17:24:22 +0200

    remove comment

--- redislock.go
+++ redislock.go
@@ -28,12 +28,6 @@ var luaPTTLScript string
 var luaObtainScript string
 
 var (
-	// 	luaObtain  = redis.NewScript(`
-	// if redis.call("set", KEYS[1], ARGV[1], "NX", "PX", ARGV[3]) then return redis.status_reply("OK") end
-
-	// local offset = tonumber(ARGV[2])
-	// if redis.call("getrange", KEYS[1], 0, offset-1) == string.sub(ARGV[1], 1, offset) then return redis.call("set", KEYS[1], ARGV[1], "PX", ARGV[3]) end
-	// `)
 	luaRefresh = redis.NewScript(luaRefeshScript)
 	luaRelease = redis.NewScript(luaReleaseScript)
 	luaPTTL    = redis.NewScript(luaPTTLScript)
