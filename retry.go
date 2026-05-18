5f567e89c38fca29393f9fe1e67eb0ddba86249c
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2026-05-18 16:48:07 +0100

    Improve tests (#87)

---
+++ retry.go
@@ -0,0 +1,76 @@
+package redislock
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+// RetryStrategy allows to customise the lock retry strategy.
+type RetryStrategy interface {
+	// NextBackoff returns the next backoff duration.
+	NextBackoff() time.Duration
+}
+
+type linearBackoff time.Duration
+
+// LinearBackoff allows retries regularly with customized intervals
+func LinearBackoff(backoff time.Duration) RetryStrategy {
+	return linearBackoff(backoff)
+}
+
+// NoRetry acquire the lock only once.
+func NoRetry() RetryStrategy {
+	return linearBackoff(0)
+}
+
+func (r linearBackoff) NextBackoff() time.Duration {
+	return time.Duration(r)
+}
+
+type limitedRetry struct {
+	s   RetryStrategy
+	cnt atomic.Int64
+	max int64
+}
+
+// LimitRetry limits the number of retries to max attempts.
+func LimitRetry(s RetryStrategy, max int) RetryStrategy {
+	return &limitedRetry{s: s, max: int64(max)}
+}
+
+func (r *limitedRetry) NextBackoff() time.Duration {
+	if r.cnt.Load() >= r.max {
+		return 0
+	}
+	r.cnt.Add(1)
+	return r.s.NextBackoff()
+}
+
+type exponentialBackoff struct {
+	cnt atomic.Uint64
+
+	min, max time.Duration
+}
+
+// ExponentialBackoff strategy is an optimization strategy with a retry time of 2**n milliseconds (n means number of times).
+// You can set a minimum and maximum value, the recommended minimum value is not less than 16ms.
+func ExponentialBackoff(min, max time.Duration) RetryStrategy {
+	return &exponentialBackoff{min: min, max: max}
+}
+
+func (r *exponentialBackoff) NextBackoff() time.Duration {
+	cnt := r.cnt.Add(1)
+
+	ms := 2 << 25
+	if cnt < 25 {
+		ms = 2 << cnt
+	}
+
+	if d := time.Duration(ms) * time.Millisecond; d < r.min {
+		return r.min
+	} else if r.max != 0 && d > r.max {
+		return r.max
+	} else {
+		return d
+	}
+}
